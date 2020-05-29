11c82ce1aee931ba66941b5222fe0604efee6e53
fiodar_marayeu <fiodar_marayeu@epam.com>
2020-05-29 20:42:19 +0300

    Logging in tests added

--- fixtures/linux_fixtures.py
+++ fixtures/linux_fixtures.py
@@ -1,5 +1,6 @@
 """Module contains fixtures for testing remote linux server."""
 
+import os
 import pytest
 from src.host_classes.linux_host import LinuxHost
 
@@ -8,4 +9,4 @@ from src.host_classes.linux_host import LinuxHost
 def host():
     """Fixture returns an LinuxHost object for its further testing."""
 
-    return LinuxHost('ec2-user', '18.156.5.31', '/home/fedor/.ssh/jenkins_access')
+    return LinuxHost('ec2-user', '18.156.5.31', os.path.expanduser('~/.ssh/jenkins_access'))
--- tests/linux_tests/test_remote_server.py
+++ tests/linux_tests/test_remote_server.py
@@ -1,29 +1,42 @@
 """Module contains tests for testing linux server."""
 
+import logging
 import pytest
 
 
 def test_cores_number(host, expected_data):
-    assert host.get_cpu_cores_number() == expected_data['cpu_cores_number']
+    logging.info(f'Actual cores number: {host.get_cpu_cores_number()},'
+                 f' expected cores number: {expected_data["cpu_cores_number"]}')
+    assert host.get_cpu_cores_number() == expected_data['cpu_cores_number'], "Incorrect number of processor cores"
 
 
 def test_network_interfaces_number(host, expected_data):
-    assert len(host.get_network_interfaces()) == expected_data['net_interfaces_number']
+    logging.info(f'Actual network interfaces number: {len(host.get_network_interfaces())},'
+                 f' expected network interfaces number: {expected_data["net_interfaces_number"]}')
+    assert len(host.get_network_interfaces()) == expected_data['net_interfaces_number'], "Incorrect number of network "\
+                                                                                         "interfaces! "
 
 
 @pytest.mark.xfail
 def test_ram_size(host, expected_data):
-    assert host.get_ram_size() >= expected_data['ram_size']
+    logging.info(f'Actual RAM size: {host.get_ram_size()}Mb, expected RAM size: {expected_data["ram_size"]}Mb')
+    assert host.get_ram_size() >= expected_data['ram_size'], "RAM size does not match!"
 
 
 @pytest.mark.xfail
 def test_partition_size(host, expected_data):
-    assert host.get_partition_size() >= expected_data['partition_size']
+    logging.info(f'Actual partition size: {host.get_partition_size()}Gb,'
+                 f' expected partition size: {expected_data["partition_size"]}Gb')
+    assert host.get_partition_size() >= expected_data['partition_size'], "Partition size does not match!"
 
 
-def test_is_chronyd_running(host):
-    assert host.is_chronyd_running
+def test_is_chronyd_running(host, expected_data):
+    logging.info(f'Actual result: chronyd is running -> {host.is_chronyd_running()}'
+                 f'expected result: chronyd is running -> {expected_data["is_chronyd_running"]}')
+    assert host.is_chronyd_running() == expected_data['is_chronyd_running'], "Chronyd is down!"
 
 
-def test_is_chronyd_enable(host):
-    assert host.is_chronyd_enable
+def test_is_chronyd_enable(host, expected_data):
+    logging.info(f'Actual result: chronyd is enabled -> {host.is_chronyd_enable()}'
+                 f'expected result: chronyd is enabled -> {expected_data["is_chronyd_enable"]}')
+    assert host.is_chronyd_enable() == expected_data['is_chronyd_enable'], "Chronyd is not enabled!"
